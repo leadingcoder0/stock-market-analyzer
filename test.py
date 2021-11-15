984af5555a8ed97ddd06cbdf020fe5d0fea04006
leadingcoder0 <59894207+leadingcoder0@users.noreply.github.com>
2021-11-15 14:49:52 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import yfinance as yf
  
-ticker_list = ['TLSA', 'AAPL']
+ticker_list = ['TSLA', 'AAPL']
+ 
 
 data = yf.download(
   tickers=ticker_list,
@@ -12,8 +13,14 @@ data = yf.download(
  
 for t in ticker_list:
   print(t)
-    
-  print(data[t]["Open"].date)
+
+  firstDataValue = data[t]["Open"][1]
+  secondDataValue = data[t]["Open"][10]
+
+  print("The price change from index 10 to 1 of " + t + " is " + str(firstDataValue) + " - " + str(secondDataValue) + " = " + str(firstDataValue - secondDataValue))
+
+  #for index, row in data[t]["Open"].iteritems():
+    #print(index, row)
  
 #for t in (data.loc["TLSA"]):
 #  print(t)
